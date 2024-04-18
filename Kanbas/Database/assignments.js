89c1240728d74dc69864b1a06c4be318ec313292
TylerChen1200 <TylerChen1200@gmail.com>
2024-04-17 22:50:04 -0400

    a5

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,11 @@
+export default [
+    { "_id": "A101", "title": "Hi TA like my assignment pls", "course": "RS101" },
+    { "_id": "A102", "title": "I enjoy writing these", "course": "RS101" },
+    { "_id": "A103", "title": "Noot noot", "course": "RS101" },
+    { "_id": "A201", "title": "Engineerringgggggg Quiz", "course": "MOI402" },
+    { "_id": "A202", "title": "What u up to", "course": "MOI402" },
+    { "_id": "A203", "title": "Jesse we need to cook!", "course": "MOI402" },
+    { "_id": "A301", "title": "Somehow Palpatine is Back", "course": "SAC103" },
+    { "_id": "A302", "title": "Theres too many of them!", "course": "SAC103" },
+    { "_id": "A303", "title": "Web Dev Exam", "course": "SAC103" }
+  ]

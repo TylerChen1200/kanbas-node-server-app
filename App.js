371e8be5f16b4b8f42b94532255518dc2c977130
TylerChen1200 <TylerChen1200@gmail.com>
2024-04-24 23:13:04 -0400

    a5

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
 import Hello from "./Hello.js";
-import Lab5 from "./Lab5.js";
-import express from 'express';
-import cors from "cors";
-import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/modules/routes.js";
-import UserRoutes from "./Users/routes.js";
+    import Lab5 from "./Lab5.js";
+    import express from 'express';
+    import cors from "cors";
+    const app = express();
 
-import mongoose from "mongoose";
-const app = express();
+    app.use(cors());
+    app.use(express.json());
+    Lab5(app);
+    Hello(app);
 
-app.use(cors());
-app.use(express.json());
-CourseRoutes(app);
-ModuleRoutes(app);
-Lab5(app);
-Hello(app);
-UserRoutes(app);
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+    app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-const port = 4000;
-app.listen(process.env.PORT || 4000);
+    const port = 4000;
+    app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`);
+    });

7ce78a7c22129973cd1f26c922554daaa8447f0d
TylerChen1200 <TylerChen1200@gmail.com>
2024-04-28 18:40:41 -0400

    yes

---
+++ App.js
@@ -0,0 +1,49 @@
+    import Hello from "./Hello.js";
+    import Lab5 from "./Lab5.js";
+    import "dotenv/config";
+    import express from 'express';
+    import cors from "cors";
+    import CourseRoutes from "./Kanbas/courses/routes.js";
+    import ModuleRoutes from "./Kanbas/modules/routes.js";
+    import mongoose from 'mongoose';
+    import UserRoutes from "./Users/routes.js";
+    import session from "express-session";
+    const app = express();
+    const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+    
+
+    const sessionOptions = {
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    };
+    app.use(
+        cors({
+          credentials: true,
+          origin: [process.env.FRONTEND_URL, "https://a6--bespoke-strudel-d0fcb8.netlify.app/", "https://a6--bespoke-strudel-d0fcb8.netlify.app/"]
+        })
+       );
+      if (process.env.NODE_ENV !== "development") {
+        sessionOptions.proxy = true;
+        sessionOptions.cookie = {
+          sameSite: "none",
+          secure: true,
+          domain: process.env.HTTP_SERVER_DOMAIN,
+        };
+      }
+      app.use(session(sessionOptions));
+      
+      
+    mongoose.connect(CONNECTION_STRING);
+    app.use(express.json());
+    UserRoutes(app);
+    CourseRoutes(app);
+    ModuleRoutes(app);
+    Lab5(app);
+    Hello(app);
+
+    
+    app.get('/', (req, res) => res.send('Hello World!'));
+    
+    const port = 4000;
+    app.listen(process.env.PORT || 4000);
